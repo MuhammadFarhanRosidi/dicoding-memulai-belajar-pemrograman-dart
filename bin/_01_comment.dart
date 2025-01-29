aba5580d0bfbdbe33c9d038a3fd4be22805976ed
MuhammadFarhanRosidi <muhfarros28@gmail.com>
2025-01-29 08:46:30 +0700

    comment

---
+++ bin/_01_comment.dart
@@ -0,0 +1,12 @@
+// single line comment
+/*
+multi
+line
+comment
+ */
+
+/// documentation comment [main] function is the entry point of the program
+/// [arguments] is a list of strings
+void main(List<String> arguments) {
+  print('Hello world');
+}
