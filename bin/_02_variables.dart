74a7aa70213ae4807d251f379b7baa28821af847
MuhammadFarhanRosidi <muhfarros28@gmail.com>
2025-01-29 14:49:11 +0700

    revisi comment in variables

--- bin/_02_variables.dart
+++ bin/_02_variables.dart
@@ -4,9 +4,10 @@ void main(List<String> args) {
   var name = 'John Doe'; // String
   var age = 30; // Integer
   var height = 1.80; // Double
+  // declare and assign value in one line
   var isMarried = false; // Boolean
   var isClever; // declare variable without value
-  // isClever = true; // assign value to variable
+  // isClever = true; // assign value to variable and when you don't assign value to variable it will be null by default
 
   // Print variables
   print('Name: $name');
