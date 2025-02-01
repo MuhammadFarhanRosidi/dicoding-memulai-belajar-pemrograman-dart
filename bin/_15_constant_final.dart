ac357ed04e8294666c5884fbb88c21f2e0fe5925
MuhammadFarhanRosidi <muhfarros28@gmail.com>
2025-02-01 13:06:07 +0700

    constant final

---
+++ bin/_15_constant_final.dart
@@ -0,0 +1,9 @@
+void main() {
+  // final variable
+  final String name = 'Bob';
+  // name = 'Alice'; // Error: a final variable can only be set once.
+
+  // constant variable
+  const String department = 'IT';
+  // department = 'HR'; // Error: a constant variable can't be set a value.
+}
