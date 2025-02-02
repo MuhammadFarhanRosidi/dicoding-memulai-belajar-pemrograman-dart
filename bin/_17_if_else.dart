86ccfabf303408116c20e138be281e33a7716e29
MuhammadFarhanRosidi <muhfarros28@gmail.com>
2025-02-02 07:51:00 +0700

    if else

---
+++ bin/_17_if_else.dart
@@ -0,0 +1,66 @@
+// void main() {
+//   bool isRainning = true;
+//   print("Hai aku akan berangkat ke sekolah, siap siap dlu ah");
+//   if (isRainning) {
+//     print("Eh ternyata hujan bro, bawa payung ah");
+//   }
+//   print("Gas berangkat dlu");
+// }
+
+// void main() {
+//   int openHours = 8;
+//   int closeHours = 23;
+//   int now = 23;
+//   if (now >= openHours && now <= closeHours) {
+//     print("Toko masih buka");
+//   } else {
+//     print("Toko sudah tutup");
+//   }
+// }
+
+// void main() {
+//   int score = 40;
+//   print('Kamu dapat predikat: ${calculateScore(score)}');
+// }
+
+// String calculateScore(int score) {
+//   if (score > 90) {
+//     return "A";
+//   } else if (score > 80) {
+//     return "B";
+//   } else if (score > 70) {
+//     return "C";
+//   } else if (score > 60) {
+//     return "D";
+//   } else if (score > 50) {
+//     return "E";
+//   } else if (score > 40) {
+//     return "F";
+//   } else {
+//     return "G";
+//   }
+// }
+
+// void main() {
+//   var shopStore = 17 < 23 ? "Buka bos" : "Tutup bos";
+//   print(shopStore);
+// }
+
+// void main() {
+//   var name = "Farhan";
+//   var buyer = name ?? "Sontal";
+//   print(buyer);
+// }
+
+void main() {
+  print("Nak tumbasno botol 1 yo nak, lek onok endok 6 yo nak");
+  print("Yo mak sek tak sandalan sek");
+  bool endok = false;
+  int botol;
+  if (endok) {
+    botol = 6;
+  } else {
+    botol = 1;
+  }
+  print("Iki mak botole jumlahe $botol yo");
+}
